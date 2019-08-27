c90992f5bec8e97a930dcb350834e94bb66810b4
201411167 <rlarlejr103@gmail.com>
2019-08-27 20:29:22 +0900

    handlebars added to this project

---
+++ src/main/java/me/minjun/springbootwebservice/web/WebController.java
@@ -0,0 +1,15 @@
+package me.minjun.springbootwebservice.web;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@AllArgsConstructor
+public class WebController {
+
+    @GetMapping("/")
+    public String main(){
+        return "main";
+    }
+}
